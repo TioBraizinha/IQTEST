8a2211f5ddb06322253602e259fa8de0a953eeed
TioBraizinha <braizinha95@gmail.com>
2025-10-22 16:05:23 +0100

    Atualização de configuração do Next.js (headers sitemap e robots)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          { key: 'Content-Type', value: 'application/xml; charset=utf-8' },
+        ],
+      },
+      {
+        source: '/robots.txt',
+        headers: [
+          { key: 'Content-Type', value: 'text/plain; charset=utf-8' },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
